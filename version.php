591f276801a2cb08482130c3f9af404fb6a475f5
hisiphp <chenf4hua12@qq.com>
2018-04-12 17:53:09 +0800

    变更版本号
    变更版本号

--- version.php
+++ version.php
@@ -3,8 +3,8 @@ return [
   'hisiphp' => 
   [
     'name' => 'HisiPHP',
-    'version' => '1.0.3',
-    'release' => 20180325,
+    'version' => '1.0.4',
+    'release' => 20180412,
     'copyright' => 'HisiPHP.com',
     'url' => 'http://www.hisiphp.com',
   ],
