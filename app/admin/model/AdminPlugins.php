ab8909626d7d2f09278b5ae798b7da9bddb4f8b6
hisiphp <chenf4hua12@qq.com>
2018-09-13 10:34:29 +0800

    [修复]插件生成控制器代码错误
    [修复]插件生成控制器代码错误，感谢QQ昵称 logger 的反馈

--- app/admin/model/AdminPlugins.php
+++ app/admin/model/AdminPlugins.php
@@ -182,12 +182,12 @@ class AdminPlugins extends Model
     public function mkExample($path = '', $data = [])
     {
         if (is_dir($path.'admin')) {
-            $admin = "<?php\nnamespace plugins\\".$data["name"]."\\admin;\nuse app\common\admin\Common;\ndefined('IN_SYSTEM') or die('Access Denied');\n\nclass Index extends Common\n{\n    public function index()\n    {\n        return ".'$this->fetch()'.";\n    }\n}";
+            $admin = "<?php\nnamespace plugins\\".$data["name"]."\\admin;\nuse app\common\controller\Common;\ndefined('IN_SYSTEM') or die('Access Denied');\n\nclass Index extends Common\n{\n    public function index()\n    {\n        return ".'$this->fetch()'.";\n    }\n}";
             file_put_contents($path . 'admin'.DS.'Index.php', $admin);
             file_put_contents($path.'view'.DS.'admin'.DS.'index'.DS.'index.php', "<?php defined('IN_SYSTEM') or die('Access Denied');/* 防止模板被盗 */?>\n{include file=\"admin@block/layui\" /}");
         }
         if (is_dir($path.'home')) {
-            $home = "<?php\nnamespace plugins\\".$data["name"]."\\home;\nuse app\common\admin\Common;\ndefined('IN_SYSTEM') or die('Access Denied');\n\nclass Index extends Common\n{\n    public function index()\n    {\n        return ".'$this->fetch()'.";\n    }\n}";
+            $home = "<?php\nnamespace plugins\\".$data["name"]."\\home;\nuse app\common\controller\Common;\ndefined('IN_SYSTEM') or die('Access Denied');\n\nclass Index extends Common\n{\n    public function index()\n    {\n        return ".'$this->fetch()'.";\n    }\n}";
             file_put_contents($path . 'home'.DS.'Index.php', $home);
             file_put_contents($path.'view'.DS.'home'.DS.'index'.DS.'index.php', '<?php defined("IN_SYSTEM") or die("Access Denied");/* 防止模板被盗 */?>');
         }
