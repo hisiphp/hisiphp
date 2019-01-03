52345d9a984ac51dd38257cd13fae89447af01e4
hisiphp <chenf4hua12@qq.com>
2019-01-03 11:56:11 +0800

    修复图标上传路径问题
    修复图标上传路径问题

--- application/install/sql/install.sql
+++ application/install/sql/install.sql
@@ -95,7 +95,7 @@ VALUES
   (45,1,'base','站点统计代码','site_statis','&lt;script&gt;alert(\'a\')&lt;/script&gt;','textarea','','','第三方流量统计代码，前台调用时请先用 htmlspecialchars_decode函数转义输出',10,1,1494691959,1494694797),
   (46,1,'base','网站名称','site_name','HisiPHP','input','','','将显示在浏览器窗口标题等位置',3,1,1494692103,1494694680),
   (47,1,'base','网站LOGO','site_logo','','image','','','网站LOGO图片',4,1,1494692345,1494693235),
-  (48,1,'base','网站图标','site_favicon','','image','','/admin/annex/favicon','又叫网站收藏夹图标，它显示位于浏览器的地址栏或者标题前面，&lt;strong class=&quot;red&quot;&gt;.ico格式&lt;/strong&gt;，&lt;a href=&quot;https://www.baidu.com/s?ie=UTF-8&amp;wd=favicon&quot; target=&quot;_blank&quot;&gt;点此了解网站图标&lt;/a&gt;',5,1,1494692781,1494693966),
+  (48,1,'base','网站图标','site_favicon','','image','','/system/annex/favicon','又叫网站收藏夹图标，它显示位于浏览器的地址栏或者标题前面，&lt;strong class=&quot;red&quot;&gt;.ico格式&lt;/strong&gt;，&lt;a href=&quot;https://www.baidu.com/s?ie=UTF-8&amp;wd=favicon&quot; target=&quot;_blank&quot;&gt;点此了解网站图标&lt;/a&gt;',5,1,1494692781,1494693966),
   (49,1,'base','手机网站','wap_site_status','1','switch','0:关闭\r\n1:开启','','如果有手机网站，请设置为开启状态，否则只显示PC网站',2,1,1498405436,1498405436),
   (50,1,'sys','云端推送','cloud_push','0','switch','0:关闭\r\n1:开启','','关闭之后，无法通过云端推送安装扩展',5,1,1504250320,1504250320),
   (51,1,'base','手机网站域名','wap_domain','ms.hisiphp.cn','input','','','手机访问将自动跳转至此域名',2,1,1504304776,1504304837),
