de3ae0da473a08e655340986aead040b20362351
hisiphp <chenf4hua12@qq.com>
2019-01-14 10:24:21 +0800

    优化通用树型类

--- extend/hisi/Tree.php
+++ extend/hisi/Tree.php
@@ -11,11 +11,10 @@
 namespace hisi;
 
 /**
-* 通用的树型类
+* 通用树型类
 */
-class Tree
-{
-
+class Tree {
+    
     /**
      * 配置参数
      * @var array
@@ -24,6 +23,7 @@ class Tree
         'id'                => 'id',        // id名称
         'pid'               => 'pid',       // pid名称
         'child'             => 'childs',    // 子元素键名
+        'name'              => 'name',      // 下拉列表的选项名
         'icon'              => '├',         // 下拉列表的图标
         'placeholder'       => '&nbsp;',    // 下拉列表的占位符
         'placeholder_number'=> 3,           // 下拉列表的占位符数量
@@ -46,30 +46,50 @@ class Tree
     public static function config($config = [])
     {
         if (!empty($config)) {
+
             return self::$config = array_merge(self::$config, $config);
+            
         }
     }
 
     /**
      * 将数据集格式化成树形结构
-     * @param array|object $data 原始数据
+     * @param array $data 原始数据
      * @param int $pid 父级id
-     * @param int $level 当前层数
+     * @param int $limitLevel  限制返回几层，0为不限制
+     * @param int $currentLevel 当前层数
      * @return array
      */
-    public static function toTree($data = [], $pid = 0, $level = 0)
+    public static function toTree($data = [], $pid = 0, $limitLevel = 0, $currentLevel = 0)
     {
         $trees = [];
         $data = array_values($data);
+        
         foreach ($data as $k => $v) {
+            
             if ($v[self::$config['pid']] == $pid) {
-                unset($data[$k]);
-                if (!empty($childs = self::toTree($data, $v[self::$config['id']], ($level+1)))) {
-                    $v[self::$config['child']] = $childs;
+
+                if ($limitLevel > 0 && $limitLevel == $currentLevel) {
+
+                    return $trees;
+                
                 }
+                
+                unset($data[$k]);
+
+                $childs = self::toTree($data, $v[self::$config['id']], $limitLevel, ($currentLevel+1));
+
+                if (!empty($childs)) {
+
+                    $v[self::$config['child']] = $childs;
+
+                }
+
                 $trees[] = $v;
             }
+
         }
+
         return $trees;
     }
 
@@ -77,34 +97,62 @@ class Tree
      * 将树形结构的数据转成下拉选择
      * @param array|object $data 原始数据
      * @param int $sid 选中ID
+     * @param array $did 禁止选择
      * @param int $level 当前层数
      * @return array
      */
-    public static function toOptions($data = [], $sid = 0, $level = 0)
+    public static function toOptions($data = [], $sid = 0, $did = [], $level = 0)
     {
+
         if (empty($data)) {
+
             return '';
+
         }
-        $str = $icon = '';
+
+        $id     = self::$config['id'];
+        $name   = self::$config['name'];
+        $str    = '';
+        $icon   = '';
+
         if ($level > 0) {
+
             for ($i=0; $i < $level; $i++) {
-            	for($j = 0; $j < self::$config['placeholder_number']; $j++) {
-                	$icon .= self::$config['placeholder'];
-            	}
+
+                for($j = 0; $j < self::$config['placeholder_number']; $j++) {
+
+                    $icon .= self::$config['placeholder'];
+
+                }
+
             }
+
             $icon .= self::$config['icon'].'&nbsp;';
         }
 
         foreach ($data as $k => $v) {
+
             if ($sid == $v['id']) {
-                $str .= '<option value="'.$v['id'].'" selected>'.$icon.$v['name'].'</option>';
+
+                $str .= '<option value="'.$v[$id].'" selected>'.$icon.$v[$name].'</option>';
+
+            } else if ($did && in_array($v[$id], $did)) {
+
+                $str .= '<option value="'.$v[$id].'" disabled>'.$icon.$v[$name].'</option>';
+
             } else {
-                $str .= '<option value="'.$v['id'].'">'.$icon.$v['name'].'</option>';
+
+                $str .= '<option value="'.$v[$id].'">'.$icon.$v[$name].'</option>';
+
             }
+
             if (isset($v['childs'])) {
-                $str.= self::toOptions($v['childs'], $sid, $level+1);
+
+                $str.= self::toOptions($v['childs'], $sid, $did, $level+1);
+
             }
         }
+
         return $str;
     }
 }
