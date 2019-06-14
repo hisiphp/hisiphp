ba31216fe362cd9aeaabb59565c7125e62d114d1
hisiphp <chenf4hua12@qq.com>
2019-06-14 11:10:40 +0800

    升级到2.0.5

--- version.php
+++ version.php
@@ -3,7 +3,7 @@ return [
   'hisiphp' => 
   [
     'name' => 'HisiPHP',
-    'version' => '2.0.4',
+    'version' => '2.0.5',
     'release' => 0,
     'copyright' => 'HisiPHP.com',
     'url' => 'http://www.hisiphp.com',
