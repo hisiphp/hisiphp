47e57b5e726facef51e1c4c35dfdd773d6ae1a76
hisiphp <chenf4hua12@qq.com>
2019-07-25 11:25:14 +0800

    更新版本号2.0.7

--- version.php
+++ version.php
@@ -3,7 +3,7 @@ return [
   'hisiphp' => 
   [
     'name' => 'HisiPHP',
-    'version' => '2.0.6',
+    'version' => '2.0.7',
     'release' => 0,
     'copyright' => 'HisiPHP.com',
     'url' => 'http://www.hisiphp.com',
