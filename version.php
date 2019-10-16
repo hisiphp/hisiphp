644eaae744dab67b912ba8878556ddd0982ebe3e
hisiphp <chenf4hua12@qq.com>
2019-10-16 23:13:18 +0800

    更新版本号

--- version.php
+++ version.php
@@ -3,7 +3,7 @@ return [
   'hisiphp' => 
   [
     'name' => 'HisiPHP',
-    'version' => '2.0.8',
+    'version' => '2.0.9',
     'release' => 0,
     'copyright' => 'HisiPHP.com',
     'url' => 'http://www.hisiphp.com',
