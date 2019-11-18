6671330a857a08a9f48bc7fda07c2cad28a7ef0a
hisiphp <chenf4hua12@qq.com>
2019-11-18 11:35:06 +0800

    新增 token 配置

---
+++ config/token.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    // 驱动方式（File、Mysql、Redis、Memcache、Memcached、Xcache）
+    'type'     => 'Mysql',
+    // 缓存前缀
+    'prefix'    => 'hisiphp',
+    // 加密算法
+    'algos'     => 'sha1',
+    // 缓存有效期 0表示永久缓存
+    'expire'   => 0,
+    // 扩展驱动
+    'extend' => ['Mysql'],
+];
