1d03797a680c6e765c3818d26f3423f71befc9bc
hisiphp <chenf4hua12@qq.com>
2019-11-18 11:53:59 +0800

    优化提示信息，避免有些迷路(找不到后台)

--- application/index/home/Error.php
+++ application/index/home/Error.php
@@ -15,6 +15,6 @@ class Error extends Common
 {
     public function _empty()
     {
-        return $this->error('这个模块是留给你自由发挥的<br>您也可以在后台指定其他模块为默认访问首页<br>路径：后台-系统-系统扩展-本地模块-设置默认模块', '', '', 100);
+        return $this->error('这个模块是留给你自由发挥的<br>您也可以在<a href="/admin.php">后台</a>指定其他模块为默认访问首页<br>路径：后台-系统-系统扩展-本地模块-设置默认模块', '', '', 100);
     }
 }
