04a34084bc177ce199da0c8d71ee0cdf38b0925d
hisiphp <chenf4hua12@qq.com>
2020-04-03 10:01:33 +0800

    更新版本号至2.0.11

--- version.php
+++ version.php
@@ -3,7 +3,7 @@ return [
   'hisiphp' => 
   [
     'name' => 'HisiPHP',
-    'version' => '2.0.10',
+    'version' => '2.0.11',
     'release' => 0,
     'copyright' => 'HisiPHP.com',
     'url' => 'http://www.hisiphp.com',
