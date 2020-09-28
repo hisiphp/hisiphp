83cc7796c820b9aec9372631b4454a64dba46203
橘子俊 <chenf4hua12@qq.com>
2020-09-28 11:02:19 +0800

    改进mysql驱动

--- thinkphp/library/think/db/connector/Mysql.php
+++ thinkphp/library/think/db/connector/Mysql.php
@@ -20,7 +20,6 @@ use think\db\Query;
  */
 class Mysql extends Connection
 {
-
     protected $builder = '\\think\\db\\builder\\Mysql';
 
     /**
@@ -97,7 +96,7 @@ class Mysql extends Connection
                 $info[$val['field']] = [
                     'name'    => $val['field'],
                     'type'    => $val['type'],
-                    'notnull' => (bool) ('' === $val['null']), // not null is empty, null is yes
+                    'notnull' => 'NO' == $val['null'],
                     'default' => $val['default'],
                     'primary' => (strtolower($val['key']) == 'pri'),
                     'autoinc' => (strtolower($val['extra']) == 'auto_increment'),
